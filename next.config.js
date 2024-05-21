35ac6dfe8e8a9eeb2fc1eb69c6504bcb0e635d77
Gregory Sanchez <gregory@dev.com>
2024-05-21 13:24:48 -0300

    Use Image Next component

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['c.biztoc.com','images.frandroid.com','securityaffairs.com', 'i0.wp.com', 'img.huffingtonpost.es'], // Agrega aquí los dominios desde donde cargarás imágenes
+  },
 }
 
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,32 @@ import { useRouter } from 'next/router'
 import PageLayout from '../components/PageLayout'
 import { useEffect, useState } from 'react'
 
-export default function Home({articles}) {
+export default function Home({topArticles}) {
+
+  const handleImg=(src)=>{
+    return src;
+  }
 
   return (
     <PageLayout>
 
         <div>
-          {articles.length === 0 && <p>no hay articulos</p>}
-          {articles.length > 0 && articles.map((article,i) => (
+          {topArticles.length === 0 && <p>no hay articulos</p>}
+          {topArticles.length > 0 && topArticles.map((article,i) => (
             <article key={i}>
                 <div> 
-                  <img alt={`Image for the article ${article.title}`} src={article.urlToImage} />
+                  {article.urlToImage && 
+                    <Image 
+                      alt={`Image for the article ${article.title}`}
+                      loader={({ src, width, quality }) => handleImg(src)}
+                      src={handleImg(article.urlToImage)}
+                      width={450}
+                      height={300}
+                      quality={50}
+                      priority={i < 2}
+                      layout='responsive'
+                      />
+                  }
                   <h2>{article.title}</h2>
                   <p>{article.description}</p>
                 </div>
@@ -29,13 +44,25 @@ export default function Home({articles}) {
   )
 }
 
+// Cuando usar staticProps o serverSideProps
+// serverSideProps se debería usar cuando: se quiera prerendizar la pag, 
+// y recuperar informacion que se tenga que hacer un fetch en el tiempo de la peticion
+// Tiene mucho sentido usar serverSideProps cuando la pagina cambia muchísimo.
+// Por ejemplo cuando se usan filtros en una pagina, es imposible calcular todos los filtros posibles
+// 
+// staticProps cuando hay resultados finitos. Cuando se sabe un numero de elementos a cargar
+// con el incrementalStaticGeneration se puede hacer que cada x tiempo, volvemos a generar 
+// esta pagina statica
+
+
 // Solo se ejecuta 1 vez en built time o al refrescar la pagina
 export async function getStaticProps() {
-  const response = await fetch('https://newsapi.org/v2/everything?q=tesla&from=2024-04-20&sortBy=publishedAt&apiKey=94086229d36b41028072ffc35a5c712a')
+  const response = await fetch('https://newsapi.org/v2/everything?q=tesla&from=2024-04-21&sortBy=publishedAt&apiKey=94086229d36b41028072ffc35a5c712a')
   const {articles} = await response.json()
+  const topArticles = articles.slice(0,3)
   return {
     props: {
-      articles
+      topArticles
     }
   }
 }
